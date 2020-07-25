56d7f47a1a1940a33b38a427beba90fcb12433f0
10836014 <10836014@ntub.edu.tw>
2020-07-26 01:16:50 +0800

    Add new api to 16.

--- object/post.php
+++ object/post.php
@@ -3,24 +3,25 @@ class Post{
  
     // database connection and table name
     private $conn;
-    private $table_name = "user";
+    private $table_name = "post";
  
     // object properties
     public $userID;
-    public $userName;
-    public $gender;
-    public $birthday;
-    public $email;
+    public $postID;
+    public $title;
+    public $content;
+    public $created_at;
+    public $updated_at;
     
     // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
     }
 
-    function read(){
+    function readPost(){
     
         // select all query
-        $query = "SELECT * FROM user ";
+        $query = "SELECT * FROM post";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
